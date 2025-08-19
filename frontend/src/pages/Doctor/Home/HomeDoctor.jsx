210f0775e1d3abb4806abffd96436c15acad5582
EricPfeuti <eric.pfeuti2007@gmail.com>
2025-08-18 21:04:30 -0300

    Arrumando Rotas Privadas - 18/08

--- frontend/src/pages/Doctor/Home/HomeDoctor.jsx
+++ frontend/src/pages/Doctor/Home/HomeDoctor.jsx
@@ -1,10 +1,8 @@
 export default function Doctor() {
-  const doctorName = localStorage.getItem("doctorName");
-
   return (
     <div>
       <h1>Área do Médico</h1>
-      <p>Bem-vindo, Dr. {doctorName} ao seu painel!</p>
+      <p>Bem-vindo, ao seu painel!</p>
     </div>
   );
 }
